98d0d940be3c36f0b704e08eada0b83367b20bf6
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-05-18 09:48:06 -0300

    Relacionamento nos models.

--- src/database/migrations/1589649594622-CreateAppointments.ts
+++ src/database/migrations/1589649594622-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1589649594622
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
