b0fd660294d4d617f687071aa0d91f9581f84af4
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-05-23 19:19:33 -0300

    Lidando com erros.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -19,22 +19,18 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentsService();
+  const createAppointment = new CreateAppointmentsService();
 
-    const appointment = await createAppointment.execute({
-      provider_id,
-      date: parsedDate,
-    });
+  const appointment = await createAppointment.execute({
+    provider_id,
+    date: parsedDate,
+  });
 
-    return response.status(201).json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.status(201).json(appointment);
 });
 
 export default appointmentsRouter;
