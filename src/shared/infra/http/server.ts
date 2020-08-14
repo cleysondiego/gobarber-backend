479f88b13e62ff31e78768fe38e7fcf05029e8ac
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2020-08-14 10:56:38 -0300

    feat: send reset password by email

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -13,7 +13,7 @@ import '@shared/container';
 const app = express();
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
